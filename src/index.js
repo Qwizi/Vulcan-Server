a4862c2400ee1778e308d0f255573cca306766e0
Adrian Ciołek <qwizi95@protonmail.com>
2020-10-08 18:02:49 +0200

    Dodanie logwania, odswiezony wyglad

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 const express = require('express')
+const session = require('express-session');
 const app = express();
 const bodyParser = require("body-parser");
 const server = require('http').createServer(app);
 const sio = require('socket.io')(server);
 const pug = require('pug');
+
+const User = require('./models/user');
+
 require('dotenv').config({path: "../.env"});
+
+const sequelize = require('./db');
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
+    //await sequelize.sync({force: true});
+    await sequelize.sync();
+    console.log("All models were synchronized successfully.");
+})();
 
 app.engine('html', pug.renderFile)
 app.set('view engine', 'pug')
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -27,4 +50,6 @@ require('./namespaces/manager')(managerNamespace, sio);
 
 server.listen(3000, () => {
     console.log('listening on http://localhost:3000');
+    const cache = require('./cache');
+    cache.set("clients", []);
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
-let clients = [];
+const bcrypt = require('bcrypt');
+const cache = require('../cache');
 
 router.get("/", (req, res) => {
-    res.render('index', {
-        clients: clients,
-        address: process.env.ADDRESS
-    })
+    if (req.session.logged) {
+        const clients = cache.get("clients");
+        res.render('index', {
+            clients: clients,
+            address: process.env.ADDRESS,
+            logged: req.session.logged,
+            user: req.session.user
+        })
+    } else {
+        res.redirect('/login')
+    }
+
 })
 
 router.get('/client/:clientId/', (req, res) => {
+    const clients = cache.get("clients");
     const clientIndex = clients.indexOf(`/clients#${req.params.clientId}`)
     const client = clients[clientIndex]
     if (!client) res.redirect(process.env.ADDRESS);
@@ -21,6 +30,41 @@ router.get('/client/:clientId/', (req, res) => {
         })
     }
 
+})
+
+router.get("/login", (req, res) => {
+    res.render('login', {
+        address: process.env.ADDRESS
+    })
+});
+
+router.post('/login', (req, res) => {
+    (async () => {
+        const User = require('../models/user');
+        const username = req.body.username;
+        const password = req.body.password;
+        if (username && password) {
+            /*bcrypt.hash(password, saltRounds, function(err, hash) {
+                const newUser = User.create({username: username, password: hash});
+            });*/
+            const user = await User.findOne({where: {username: username}});
+            if (user instanceof User) {
+                const matchedPassword = await bcrypt.compare(password, user.dataValues.password);
+                if (matchedPassword) {
+                    req.session.logged = true;
+                    req.session.user = user.dataValues;
+                    res.redirect('/');
+                }
+            }
+        }
+        res.redirect('/login');
+    })();
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(function (err) {
+        res.redirect('/'); //Inside a callback… bulletproof!
+    });
 })
 
 module.exports = router;
